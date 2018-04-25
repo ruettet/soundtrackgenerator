3ac0c42b3ba57828eadb7e81bb187c06139a7700
ruettet <tom.ruette@gmail.com>
2018-04-25 22:09:06 +0200

    this is obviously the wrong way to intertwine sound

--- soundtrackgenerator.py
+++ soundtrackgenerator.py
@@ -1,5 +1,23 @@
 import numpy as np
 import cv2
+import pyaudio
+import math
+
+
+def sine(frequency, length, rate):
+    length = int(length * rate)
+    factor = float(frequency) * (math.pi * 2) / rate
+    return np.sin(np.arange(length) * factor)
+
+
+def play_tone(stream, frequency=440, length=1, rate=44100):
+    chunks = []
+    chunks.append(sine(frequency, length, rate))
+    chunk = np.concatenate(chunks) * 0.25
+    stream.write(chunk.astype(np.float32).tostring())
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=1)
 
 cap = cv2.VideoCapture(0)
 
@@ -21,8 +39,10 @@ while(True):
     count = mask.sum()
 
     if count > yellow_threshold:
+        play_tone(stream, frequency=880)
         print('I can see yellow')
     else:
+        play_tone(stream)
         print('I see no yellow')
 
     cv2.imshow('frame', frame)
@@ -34,3 +54,7 @@ while(True):
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+# close everything audio related
+stream.close()
+p.terminate()
